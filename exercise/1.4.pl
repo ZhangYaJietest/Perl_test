79bb6e78e7a16ced0e6bca0216c7ff2af0155c4e
ZhangYaJietest <49471452+ZhangYaJietest@users.noreply.github.com>
2021-09-03 10:38:03 +0800

    Create 1.4.pl

---
+++ exercise/1.4.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+
+my  $var1 = 5;
+my  $var2 = 4;
+
+my  $add = $var1+$var2;
+print "Add: ".$add."\n";
+
+my $Sub = $var1-$var2;
+print "Sub: ".$Sub."\n";
+
+my $Mult = $var1*$var2;
+print "mult:".$Mult."\n";
+
+my $Div = $var1/$var2;
+print "Div: ".$Div."\n";
